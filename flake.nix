d86dccbc3f8ddcb24820343840ae46c1734726ff
darbster145 <110804583+darbster145@users.noreply.github.com>
2024-08-28 22:33:28 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,15 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    apple-silicon = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... } @ inputs: 
-    {
-      nixosConfigurations.brixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-	 ./configuration.nix
-	];
+  outputs = { nixpkgs, ... } @ inputs: {
+      nixosConfigurations = {
+        brixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [ ./configuration.nix ];
+        };
+        nixi = nixpkgs.lib.nixosSystem {
+          system = "aarch64_linux";
+          modules = [ ./configuration.nix ];
+        };
       };
-  };
+   };
+ };
 }
+
