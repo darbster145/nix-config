3a63a47b699912717a347c62df6776b2c87a860d
darbster145 <swoopmaster22@proton.me>
2024-08-29 21:44:14 -0600

    added some system packages

--- hosts/nixi/configuration.nix
+++ hosts/nixi/configuration.nix
@@ -89,7 +89,7 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    firefox
+    firefox-devedition
     kitty 
     htop
     fastfetch
