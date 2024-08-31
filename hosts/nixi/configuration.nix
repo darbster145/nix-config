bd687eb4d46fc011899714011555c02ba1e94cad
darbster145 <swoopmaster22@proton.me>
2024-08-30 23:22:01 -0600

    added hyprland

--- hosts/nixi/configuration.nix
+++ hosts/nixi/configuration.nix
@@ -56,6 +56,8 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  programs.hyprland.enable = true;
+
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
