9399d76ba3c9b83b7f49ead943666a243c3506d0
darbster145 <swoopmaster22@proton.me>
2024-09-04 08:06:21 -0600

    Added a few things

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     apple-silicon.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
   };
 
   outputs = { nixpkgs, ... } @ inputs: {
--- nixos/hosts/nixi/configuration.nix
+++ nixos/hosts/nixi/configuration.nix
@@ -34,10 +34,6 @@
   # Set your time zone.
   time.timeZone = "America/Denver";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
@@ -65,15 +61,7 @@
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+  services.printing.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
@@ -87,10 +75,13 @@
     ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  security.sudo.wheelNeedsPassword = false;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # System Packages
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
     wget
     git
     firefox-devedition
@@ -119,21 +110,21 @@
     openconnect
     openconnect_openssl
     lazygit
-
+    ungoogled-chromium
+    youtube-music
     # Hyperland Programs
     waybar
     brightnessctl
     pavucontrol
-    
-
   ];
 
+  # Enable Appimages
   programs.appimage = {
     enable = true;
     binfmt = true;
   };
 
-
+  # Fonts
   fonts.packages = with pkgs; [
     fira-code
     font-awesome
@@ -152,38 +143,18 @@
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
+  # SSH 
   services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  # DO NOT CHANGE
+  system.stateVersion = "24.11";
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  # Enable nix experimental features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
