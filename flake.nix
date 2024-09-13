825828c09c54154087e3a9eb9b7671be05587e61
darbster145 <swoopmaster22@proton.me>
2024-09-12 19:25:40 -0600

    merged nix-darwin

--- flake.nix
+++ flake.nix
@@ -1,19 +1,41 @@
 {
-  description = "A 'very' basic flake";
+  description = "Combined flake for Darwin and NixOS systems";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     apple-silicon.url = "github:tpwrules/nixos-apple-silicon";
     home-manager.url = "github:nix-community/home-manager/master";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    spacebar.url = "github:cmacrae/spacebar/v1.4.0";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Homebrew taps for crapple
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+    AeroSpace = {
+      url = "github:nikitabobko/homebrew-tap";
+      flake = false;
+    };
   };
 
-  outputs = { nixpkgs, nix-flatpak, apple-silicon, ... } @ inputs: {
+  outputs = { self, nixpkgs, apple-silicon, nix-flatpak, nix-darwin, nix-homebrew, ... } @ inputs: {
+
     nixosConfigurations = {
       brixos = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         system = "x86_64-linux";
-        modules = [ 
+        modules = [
           ./nixos/hosts/brixos/configuration.nix
           nix-flatpak.nixosModules.nix-flatpak
         ];
@@ -21,12 +43,36 @@
       nixi = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         system = "aarch64-linux";
-        modules = [ 
+        modules = [
           ./nixos/hosts/nixi/configuration.nix
           nix-flatpak.nixosModules.nix-flatpak
           apple-silicon.nixosModules.apple-silicon-support
         ];
       };
+    };
+
+    darwinConfigurations = {
+      "MacBook-Air-3" = nix-darwin.lib.darwinSystem {
+        specialArgs = { inherit inputs; };
+        system = "aarch64-darwin";
+        modules = [
+          ./darwin/hosts/macbook-air-3/configuration.nix
+        ];
+      };
+
+      crapple = nix-darwin.lib.darwinSystem {
+        specialArgs = { inherit inputs; };
+        system = "aarch64-darwin";
+        modules = [
+          ./darwin/hosts/crapple/configuration.nix
+          nix-homebrew.darwinModules.nix-homebrew
+        ];
+      };
+    };
+
+    darwinPackages = {
+      "MacBook-Air-3" = self.darwinConfigurations."MacBook-Air-3".pkgs;
+      crapple = self.darwinConfigurations.crapple.pkgs;
     };
   };
 }
