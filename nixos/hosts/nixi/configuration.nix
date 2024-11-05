bc84a9c6e8baab5717f1f2472f51c07dc9dd787f
darbster145 <swoopmaster22@proton.me>
2024-11-04 18:09:16 -0700

    added kanata service and config

--- nixos/hosts/nixi/configuration.nix
+++ nixos/hosts/nixi/configuration.nix
@@ -25,6 +25,15 @@
   };
   boot.loader.efi.canTouchEfiVariables = false;
 
+  # Enable zram swap
+  zramSwap.enable = true;
+
+  services.logind.extraConfig = ''
+    # don’t shutdown when power button is short-pressed
+    HandlePowerKey=ignore
+  '';
+
+
   networking.hostName = "nixi"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -82,6 +91,43 @@
   # Enable tailscale
   services.tailscale.enable = true;
 
+  # Enable kanata service
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        devices = [
+          "/dev/input/by-path/platform-39b10c000.spi-cs-0-event-kbd"
+        ];
+        extraDefCfg = "process-unmapped-keys yes";
+        config = ''
+          (defsrc
+           caps a s d f j k l ;
+          )
+          (defvar
+           tap-time 150
+           hold-time 200
+          )
+          (defalias
+           caps (tap-hold 100 100 esc lctl)
+           a (tap-hold $tap-time $hold-time a lmet)
+           s (tap-hold $tap-time $hold-time s lalt)
+           d (tap-hold $tap-time $hold-time d lsft)
+           f (tap-hold $tap-time $hold-time f lctl)
+           j (tap-hold $tap-time $hold-time j rctl)
+           k (tap-hold $tap-time $hold-time k rsft)
+           l (tap-hold $tap-time $hold-time l ralt)
+           ; (tap-hold $tap-time $hold-time ; rmet)
+          )
+
+          (deflayer base
+           @caps @a  @s  @d  @f  @j  @k  @l  @;
+          )
+        '';
+      };
+    };
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.brad = {
     isNormalUser = true;
@@ -133,6 +179,7 @@
     adoptopenjdk-icedtea-web
     _1password-gui-beta
     _1password-cli
+    #inputs.zen-browser.packages."${system}".specific
 
     # Hyperland Programs
     waybar
@@ -146,7 +193,7 @@
     tangram
     calls
     linphone
-    inputs.zen-browser.packages."${system}".specific
+
   ];
 
   services.flatpak = {
