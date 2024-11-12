7d52984a55525e32a167370b8aa05dca59138299
darbster145 <swoopmaster22@proton.me>
2024-11-12 09:19:33 -0700

    starting point

--- flake.nix
+++ flake.nix
@@ -1,83 +1,79 @@
 {
-  description = "Combined flake for NixOS and Darwin systems";
+  description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    apple-silicon.url = "github:tpwrules/nixos-apple-silicon";
-    home-manager.url = "github:nix-community/home-manager/master";
-    nix-flatpak.url = "github:gmodena/nix-flatpak";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    spacebar.url = "github:cmacrae/spacebar/v1.4.0";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    firefox.url = "github:nix-community/flake-firefox-nightly";
-    firefox.inputs.nixpkgs.follows = "nixpkgs";
-    zen-browser.url = "github:ch4og/zen-browser-flake";
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
-
-    # Optional: Homebrew taps for crapple
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
-    };
-    AeroSpace = {
-      url = "github:nikitabobko/homebrew-tap";
-      flake = false;
-    };
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, apple-silicon, nix-flatpak, nix-darwin, nix-homebrew, firefox, ... } @ inputs: {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    # Your custom packages
+    # Accessible through 'nix build', 'nix shell', etc
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    # Formatter for your nix files, available through 'nix fmt'
+    # Other options beside 'alejandra' include 'nixpkgs-fmt'
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
+    # Your custom packages and modifications, exported as overlays
+    overlays = import ./overlays {inherit inputs;};
+    # Reusable nixos modules you might want to export
+    # These are usually stuff you would upstream into nixpkgs
+    nixosModules = import ./modules/nixos;
+    # Reusable home-manager modules you might want to export
+    # These are usually stuff you would upstream into home-manager
+    homeManagerModules = import ./modules/home-manager;
+
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
-      brixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
+      # FIXME replace with your hostname
+      your-hostname = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
         modules = [
-          ./nixos/hosts/brixos/configuration.nix
-          nix-flatpak.nixosModules.nix-flatpak
-        ];
-      };
-      nixi = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "aarch64-linux";
-        modules = [
-          ./nixos/hosts/nixi/configuration.nix
-          nix-flatpak.nixosModules.nix-flatpak
-          apple-silicon.nixosModules.apple-silicon-support
+          # > Our main nixos configuration file <
+          ./nixos/configuration.nix
         ];
       };
     };
 
-    darwinConfigurations = {
-      "JRSYS-MBA-01" = nix-darwin.lib.darwinSystem {
-        specialArgs = { inherit inputs; };
-        system = "aarch64-darwin";
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      # FIXME replace with your username@hostname
+      "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
         modules = [
-          ./darwin/hosts/jrsys-mba-01/configuration.nix
+          # > Our main home-manager configuration file <
+          ./home-manager/home.nix
         ];
       };
-
-      crapple = nix-darwin.lib.darwinSystem {
-        specialArgs = { inherit inputs; };
-        system = "aarch64-darwin";
-        modules = [
-          ./darwin/hosts/crapple/configuration.nix
-          nix-homebrew.darwinModules.nix-homebrew
-        ];
-      };
-    };
-
-    darwinPackages = {
-      "JRSYS-MBA-01" = self.darwinConfigurations."JRSYS-MBA-01".pkgs;
-      crapple = self.darwinConfigurations.crapple.pkgs;
     };
   };
 }
-
