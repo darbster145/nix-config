645e6c7310252de7babdea297e7883459956e59a
darbster145 <swoopmaster22@proton.me>
2025-10-25 14:11:44 -0600

    nix flake update nixi, disabled walker until elephant dependency update

--- hosts/nixos/features/hyprland.nix
+++ hosts/nixos/features/hyprland.nix
@@ -50,10 +50,10 @@
     kdePackages.kwallet-pam
   ];
 
-  programs.walker = {
-    enable = true;
-    runAsService = true;
-  };
+  #programs.walker = {
+  #  enable = true;
+  #  #runAsService = true;
+  #};
 
   services.gvfs.enable = true;
   services.tumbler.enable = true;
