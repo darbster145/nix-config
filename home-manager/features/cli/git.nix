8688c9d7180acbd871cb29293ef89e5133d5fb79
darbster145 <swoopmaster22@proton.me>
2025-10-28 13:10:30 -0600

    Reapply local JRSYS-MBA01 changes after pull; keep upstream flake.lock

--- home-manager/features/cli/git.nix
+++ home-manager/features/cli/git.nix
@@ -3,47 +3,51 @@
 {
   programs.git = {
     enable = true;
-    userEmail = "swoopmaster22@proton.me";
-    userName = "darbster145";
-    aliases = {
-      gp = "git pull";
-      gc = "git commit";
-      gs = "git status";
-    };
+    settings = {
+      user = {
+        name = "darbster145";
+        email = "swoopmaster22@proton.me";
+      };
+      aliases = {
+        gp = "git pull";
+        gc = "git commit";
+        gs = "git status";
+      };
 
-    extraConfig = {
-      core = {
-        compression = 9;
-        whitespace = "error";
-        preloadindex = true;
-      };
-      advice = {
-        addEmptyPathspec = false;
-        pushNonFastForward = false;
-        statusHints = false;
-      };
-      status = {
-        branch = true;
-        showStash = true;
-        showUntrackedFiles = true;
-      };
-      diff = {
-        context = 3;
-        renames = "copies";
-        interHunkContext = 10;
-      };
-      push = {
-        autoSetupRemote = true;
-        default = "current";
-        followTags = true;
-      };
-      pull = {
-        default = "current";
-        rebase = true;
-      };
-      rebase = {
-        autoStash = true;
-        missingCommitsCheck = "warn";
+      extraConfig = {
+        core = {
+          compression = 9;
+          whitespace = "error";
+          preloadindex = true;
+        };
+        advice = {
+          addEmptyPathspec = false;
+          pushNonFastForward = false;
+          statusHints = false;
+        };
+        status = {
+          branch = true;
+          showStash = true;
+          showUntrackedFiles = true;
+        };
+        diff = {
+          context = 3;
+          renames = "copies";
+          interHunkContext = 10;
+        };
+        push = {
+          autoSetupRemote = true;
+          default = "current";
+          followTags = true;
+        };
+        pull = {
+          default = "current";
+          rebase = true;
+        };
+        rebase = {
+          autoStash = true;
+          missingCommitsCheck = "warn";
+        };
       };
     };
   };
--- home-manager/global/default.nix
+++ home-manager/global/default.nix
@@ -58,15 +58,9 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git =
-    {
-      enable = true;
-      userEmail = "swoopmaster22@proton.me";
-      userName = "darbster145";
-    }
 
-      # Nicely reload system units when changing configs
-      systemd.user.startServices = "sd-switch";
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
